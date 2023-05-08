f5c0b3ebdd8986dc36d9e52f9ffd0783f03cbf00
TomasAMolinari <49258965+TomasAMolinari@users.noreply.github.com>
2023-05-08 17:51:49 -0300

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -24,7 +24,7 @@ resource "azurerm_service_plan" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type	      = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_web_app" "example" {
