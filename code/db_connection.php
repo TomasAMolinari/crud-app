7dc781e133748be7a66b09e3789e63d560879cf5
TomasAMolinari <49258965+TomasAMolinari@users.noreply.github.com>
2023-05-09 15:01:46 -0300

    Update db_connection.php

--- code/db_connection.php
+++ code/db_connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "crud_app";
+$__servername__ = "localhost";
+$__username__ = "root";
+$__password__ = "";
+$__db_name__ = "crud_app";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $db_name);
+$conn = mysqli_connect($__servername__, $__username__, $__password__, $__db_name__);
 
 // Check connection
 if (!$conn) {
