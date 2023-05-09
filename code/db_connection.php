db625a1150a778252aab8cc763bcd2f5f2eb0607
TomasAMolinari <49258965+TomasAMolinari@users.noreply.github.com>
2023-05-09 17:23:00 -0300

    Update db_connection.php

--- code/db_connection.php
+++ code/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$__servername__ = "localhost";
-$__username__ = "root";
-$__password__ = "";
-$__db_name__ = "crud_app";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "crud_app";
 
 // Create connection
 $conn = mysqli_connect($__servername__, $__username__, $__password__, $__db_name__);
